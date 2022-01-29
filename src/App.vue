281ce136aed78b86ca523b71a8d8292827c96efb
Matus <kobe21@gmail.com>
2022-01-29 14:33:05 +0100

    wip

--- src/App.vue
+++ src/App.vue
@@ -3,9 +3,16 @@
     <router-link to="/">Home</router-link> |
     <router-link to="/about">About</router-link>
   </div>
-  <div id="sidebar">team ecommerce</div>
-  <div id="content">
-    <router-view />
+  <div class="flex">
+    <div id="sidebar" class="w-64 bg-gray-400">
+      <a href="#" class="block">Block</a>
+      <a href="#">Block</a>
+      <a href="#">Block</a>
+      <a href="#">Block</a>
+    </div>
+    <div id="content" class="text-sm">
+      <router-view />
+    </div>
   </div>
 </template>
 
