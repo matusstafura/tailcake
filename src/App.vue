98bd5d4af53f83645b03bb76f9472500cc0635d0
Matus <kobe21@gmail.com>
2022-01-29 11:38:54 +0100

    wip

--- src/App.vue
+++ src/App.vue
@@ -3,28 +3,14 @@
     <router-link to="/">Home</router-link> |
     <router-link to="/about">About</router-link>
   </div>
-  <router-view />
+  <div id="sidebar">team ecommerce</div>
+  <div id="content">
+    <router-view />
+  </div>
 </template>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
-
 #nav {
-  padding: 30px;
-}
-
-#nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
-
-#nav a.router-link-exact-active {
-  color: #42b983;
+  background: yellowgreen;
 }
 </style>
