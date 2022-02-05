0e0ba8c0d37567da36a3e7b35271755d045d712e
Matus <kobe21@gmail.com>
2022-02-05 09:07:46 +0100

    highlightjs

--- src/components/Button2.vue
+++ src/components/Button2.vue
@@ -3,19 +3,17 @@
     <div class="flex mb-4 justify-between">
       <h2 class="text-xl font-semibold justify-start">{{ title }}</h2>
       <div class="mt-0">
-        <button @click="showCode">Show Code</button>
+        <button>RTA(if true)</button>
+        <button @click="showCode" class="ml-4">Show Code</button>
         <button @click="darkMode" class="ml-4">Dark Mode</button>
+        <button class="ml-4">Copy Code</button>
       </div>
     </div>
-    <div class="bg-gray-100 p-10 flex">
+    <div class="bg-gxray-100 p-10 flex language-html">
       <div v-html="code"></div>
     </div>
     <div v-show="active">
-      <pre>
-        <code>
-          {{ code }}
-        </code>
-      </pre>
+      <highlightjs language="js" :code="code" class="text-sm" />
     </div>
     <div></div>
   </div>
@@ -26,8 +24,7 @@ export default {
   name: "Button2",
   data() {
     return {
-      code: `
-<button class="bg-blue-400 dark:bg-white dark:text-black px-6 py-3 text-white font-bold uppercase rounded shadow-lg hover:bg-blue-500 focus:bg-blue-700" > 
+      code: `<button class="bg-blue-400 dark:bg-white dark:text-black px-6 py-3 text-white font-bold uppercase rounded shadow-lg hover:bg-blue-500 focus:bg-blue-700">
   Button
 </button>`,
       active: false,
--- src/main.js
+++ src/main.js
@@ -1,6 +1,9 @@
 import { createApp } from "vue";
 import App from "./App.vue";
 import router from "./router";
+import "highlight.js/styles/stackoverflow-light.css";
+import "highlight.js/lib/common";
+import hljsVuePlugin from "@highlightjs/vue-plugin";
 import "./assets/css/input.css";
 
-createApp(App).use(router).mount("#app");
+createApp(App).use(router).use(hljsVuePlugin).mount("#app");
