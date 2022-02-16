dd1bf01a6109deee668fe44c5c5c5a06d44ab6e3
Matus <kobe21@gmail.com>
2022-02-16 13:11:42 +0100

    wip

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -1,15 +1,19 @@
 <template>
   <Navbar1></Navbar1>
   <Navbar2></Navbar2>
+  <Navbar3></Navbar3>
 </template>
 
 <script>
 import Navbar1 from "./Navbars/Navbar1.vue";
 import Navbar2 from "./Navbars/Navbar2.vue";
+import Navbar3 from "./Navbars/Navbar3.vue";
+
 export default {
   components: {
     Navbar1,
     Navbar2,
+    Navbar3,
   },
 };
 </script>
---
+++ src/components/Navbars/Navbar3.vue
@@ -0,0 +1,60 @@
+<template>
+  <Element :title="title" :code="code"></Element>
+</template>
+
+<script>
+import Element from "../Element.vue";
+export default {
+  components: { Element },
+  name: "Navbar2",
+  data() {
+    return {
+      code: `<header class="bg-gray-100 shadow dark:bg-gray-700">
+
+<div class="flex items-center justify-start px-4 py-4">
+
+<nav class="hidden dark:text-white lg:block">
+<div class="flex items-center">
+  <a href="#" class="flex p-2 px-4 font-bold rounded hover:bg-blue-300 hover:text-white">New
+  <svg class="w-4 ml-3" xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" viewBox="0 0 20 20" fill="currentColor">
+  <path fillRule="evenodd" d="M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z" clipRule="evenodd" />
+  </svg>
+  </a>
+  <a href="#" class="flex p-2 px-4 font-bold rounded hover:bg-blue-300 hover:text-white">Clothing
+  <svg class="w-4 ml-3" xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" viewBox="0 0 20 20" fill="currentColor">
+  <path fillRule="evenodd" d="M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z" clipRule="evenodd" />
+  </svg>
+  </a>
+  <a href="#" class="flex p-2 px-4 font-bold rounded hover:bg-blue-300 hover:text-white">Sportswear
+  <svg class="w-4 ml-3" xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" viewBox="0 0 20 20" fill="currentColor">
+  <path fillRule="evenodd" d="M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z" clipRule="evenodd" />
+  </svg>
+  </a>
+  <a href="#" class="flex p-2 px-4 font-bold rounded hover:bg-blue-300 hover:text-white">Underwear
+  <svg class="w-4 ml-3" xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" viewBox="0 0 20 20" fill="currentColor">
+  <path fillRule="evenodd" d="M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z" clipRule="evenodd" />
+  </svg>
+  </a>
+  <a href="#" class="flex p-2 px-4 font-bold rounded hover:bg-blue-300 hover:text-white">Trending
+  <svg class="w-4 ml-3" xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" viewBox="0 0 20 20" fill="currentColor">
+  <path fillRule="evenodd" d="M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z" clipRule="evenodd" />
+  </svg>
+  </a>
+  <a href="#" class="flex p-2 px-4 font-bold rounded hover:bg-blue-300 hover:text-white">Brands
+  <svg class="w-4 ml-3" xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" viewBox="0 0 20 20" fill="currentColor">
+  <path fillRule="evenodd" d="M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z" clipRule="evenodd" />
+  </svg>
+  </a>
+<div>
+</nav>
+
+</div>
+
+</header>`,
+      title: "Navbar basic",
+    };
+  },
+};
+</script>
+
+<style scoped></style>
