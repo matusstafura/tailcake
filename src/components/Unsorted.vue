ecaa9fce9e7df4ab06162e1cfbbd73e4eae3bafc
Matus <kobe21@gmail.com>
2022-03-05 16:17:19 +0100

    add CTA

--- src/components/Unsorted.vue
+++ src/components/Unsorted.vue
@@ -1,15 +1,19 @@
 <template>
   <div>
     <Pricing1></Pricing1>
+    <Cta1></Cta1>
+    <Cta2></Cta2>
   </div>
 </template>
 
 <script>
 import Pricing1 from "./Unsorted/Pricing1.vue";
+import Cta1 from "./Unsorted/Cta1.vue";
+import Cta2 from "./Unsorted/Cta2.vue";
 export default {
   name: "Unsorted",
   props: ["mode"],
-  components: { Pricing1 },
+  components: { Pricing1, Cta1, Cta2 },
 };
 </script>
 
---
+++ src/components/Unsorted/Cta1.vue
@@ -0,0 +1,35 @@
+<template>
+  <Element :title="title" :code="code"></Element>
+</template>
+
+<script>
+import Element from "../Element.vue";
+export default {
+  components: { Element },
+  name: "Cta 1",
+  data() {
+    return {
+      code: `<!-- CTA -->
+<section class="m-8 bg-white border rounded-lg shadow-xl border-gray-50 dark:bg-gray-500 dark:text-white">
+
+<div class="flex p-8">
+  <div class="flex w-8/12">
+    <h3 class="m-auto text-4xl font-semibold">Ready to try our award winning service</h3>
+  </div>
+  <div class="m-auto text-center">
+    <div class="mb-4">
+    <button class="flex px-6 py-3 font-light text-white bg-green-400 rounded shadow-lg hover:bg-blue-500">
+      <span class="mx-1">Get Started Now</span>
+    </button>
+    </div>
+    <a href="#" class="text-sm text-gray-400 underline">Read details</a>
+  </div>
+</div>
+</section>`,
+      title: "Cta 1",
+    };
+  },
+};
+</script>
+
+<style scoped></style>
