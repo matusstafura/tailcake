2f6dab27c10a473102b32ab0805523e067f06784
Matus Stafura <kobe21@gmail.com>
2022-05-12 09:03:45 +0200

    update breadcrumbs; add buttons

--- src/components/Breadcrumbs/Breadcrumb1.vue
+++ src/components/Breadcrumbs/Breadcrumb1.vue
@@ -11,10 +11,12 @@ export default {
     return {
       code: `<div class="w-full px-4 py-4 text-sm font-light text-gray-700 capitalize bg-gray-100 dark:bg-gray-500 dark:text-gray-100">
   <a href="#" class="hover:text-blue-500 dark:hover:text-blue-300">Home</a>
-  <span class="mx-2">></span>
+  <span class="mx-1">›</span>
   <a href="#" class="hover:text-blue-500 dark:hover:text-blue-300">Hobby</a>
-  <span class="mx-2">></span>
-  <a href="#" class="hover:text-blue-500 dark:hover:text-blue-300">Crafting</a>
+  <span class="mx-1">›</span>
+  <a href="#" class="hover:text-blue-500 dark:hover:text-blue-300">Hand Tools</a>
+  <span class="mx-1">›</span>
+  <a href="#" class="hover:text-blue-500 dark:hover:text-blue-300">Accessories</a>
 </div>`,
       title: "Arrow",
     };
--- src/components/Buttons/Button1.vue
+++ src/components/Buttons/Button1.vue
@@ -6,16 +6,13 @@
 import Element from "../Element.vue";
 export default {
   components: { Element },
-  name: "Add To Cart Button",
+  name: "Primary Button",
   data() {
     return {
-      code: `<button class="flex px-6 py-3 font-light text-white bg-blue-400 rounded shadow-lg hover:bg-blue-500">
-      <svg xmlns="http://www.w3.org/2000/svg" class="w-5 h-5" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-  <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M16 11V7a4 4 0 00-8 0v4M5 9h14l1 12H4L5 9z" />
-</svg>
-  <span class="mx-1">Add To Cart</span>
+      code: `<button class="px-6 py-3 font-bold text-white uppercase bg-blue-400 rounded shadow-lg dark:bg-white dark:text-black hover:bg-blue-500">
+  Button
 </button>`,
-      title: "Add To Cart Button",
+      title: "Primary Button",
     };
   },
 };
