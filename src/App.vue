b76e32db36fbbef55171bacf62b9fd9dbf1ce864
Matus Stafura <kobe21@gmail.com>
2022-05-21 19:05:43 +0200

    responsive sidebar

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,25 @@
 <template>
   <div class="bg-gradient-to-r from-blue-50 to-teal-50 via-gray-50">
     <div id="nav" class="flex justify-between px-8 py-8">
+      <a href="#" class="flex text-gray-400" @click="toggleMenu">
+        <svg
+          class="lg:hidden"
+          fill="hsl(224, 7.2%, 40%)"
+          viewBox="0 0 100 80"
+          width="32"
+          height="32"
+        >
+          <rect width="80" height="14"></rect>
+          <rect y="30" width="80" height="14"></rect>
+          <rect y="60" width="80" height="14"></rect>
+        </svg>
+      </a>
       <Menu></Menu>
     </div>
     <div class="flex">
-      <Sidebar></Sidebar>
+      <div :class="menu ? 'hidden lg:block' : 'block'">
+        <Sidebar></Sidebar>
+      </div>
       <div id="content" class="w-full px-8 py-4">
         <router-view></router-view>
       </div>
@@ -20,6 +35,17 @@ export default {
   components: {
     Sidebar,
     Menu,
+  },
+  data() {
+    return {
+      menu: false,
+    };
+  },
+  methods: {
+    toggleMenu() {
+      this.menu = !this.menu;
+      console.log(this.menu);
+    },
   },
 };
 </script>
