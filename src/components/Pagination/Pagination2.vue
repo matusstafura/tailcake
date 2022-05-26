0848ea4bbaf1834e47d52d9f4cee598fa4e5b7e7
Matus Stafura <kobe21@gmail.com>
2022-05-26 10:45:09 +0200

    add pagination full width

--- src/components/Pagination/Pagination2.vue
+++ src/components/Pagination/Pagination2.vue
@@ -6,25 +6,30 @@
 import Element from "../Element.vue";
 export default {
   components: { Element },
-  name: "Basic",
+  name: "Full width",
   data() {
     return {
       code: `
-<div class="flex justify-center">
-  <nav class="w-full">
-    <ul class="flex justify-center py-2 text-gray-500 dark:text-gray-300 list-style-none">
-      <li><a href="#" class="px-3 hover:underline">Previous</a></li>
+<div class="mx-4 text-sm text-gray-500 dark:text-gray-300">
+  <hr class="w-full border-t-2 border-gray-100" />
+  <div class="flex justify-center px-4 py-2">
+  <div><a href="#" class="hover:underline">Previous</a></div>
+  <nav class="w-full bg-red">
+    <ul class="flex justify-center list-style-none">
       <li><a href="#" class="p-2 px-3 rounded hover:bg-blue-300 hover:text-white">1</a></li>
       <li><a href="#" class="p-2 px-3 rounded hover:bg-blue-300 hover:text-white">2</a></li>
       <li><span class="p-2 px-3 font-bold text-blue-400">3</span></li>
       <li><a href="#" class="p-2 px-3 rounded hover:bg-blue-300 hover:text-white">4</a></li>
-      <li><a href="#" class="p-2 px-3 rounded hover:bg-blue-300 hover:text-white">5</a></li>
-      <li><a href="#" class="px-3 hover:underline">Next</a></li>
+      <li><a href="#" class="p-2 px-3 rounded hover:bg-blue-300 hover:text-white">5</a></li>     
     </ul>
   </nav>
+  <div>
+    <a href="#" class="flex hover:underline">Next</a>
+  </div>
+  </div>
 </div>
 `,
-      title: "Basic",
+      title: "Full width",
     };
   },
 };
