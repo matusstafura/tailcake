807690cccbc73089e95774bcd7a7d583732181e4
Matus Stafura <kobe21@gmail.com>
2022-05-28 14:09:20 +0200

    add pagination; deprecated creator

--- src/router/index.js
+++ src/router/index.js
@@ -12,97 +12,86 @@ import Content from "@/components/Content";
 import Pagination from "@/components/Pagination";
 import Cta from "@/components/Cta";
 import PageNotFound from "../views/PageNotFound.vue";
-import Creator from "@/views/Creator.vue";
 
-const routes = [
-  {
-    path: "/",
-    name: "Home",
-    component: Home,
-  },
-  {
-    path: "/alerts",
-    name: "alerts",
-    component: Alerts,
-  },
-  {
-    path: "/buttons",
-    name: "buttons",
-    component: Button,
-  },
-  {
-    path: "/breadcrumbs",
-    name: "breadcrumbs",
-    component: Breadcrumbs,
-  },
-  {
-    path: "/commerce",
-    name: "commerce",
-    component: Commerce,
-  },
-  {
-    path: "/content",
-    name: "content",
-    component: Content,
-  },
-  {
-    path: "/cta",
-    name: "cta",
-    component: Cta,
-  },
-  {
-    path: "/footer",
-    name: "footer",
-    component: Footer,
-  },
-  {
-    path: "/blog",
-    name: "blog",
-    component: Blog,
-  },
-  {
-    path: "/navbars",
-    name: "navbars",
-    component: Navbar,
-  },
-  {
-    path: "/pagination",
-    name: "pagination",
-    component: Pagination,
-  },
-  {
-    path: "/tables",
-    name: "tables",
-    component: Table,
-  },
-  {
-    path: restrict(),
-    name: "creator",
-    component: Creator,
-  },
-  {
-    path: "/about",
-    name: "About",
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () =>
-      import(/* webpackChunkName: "about" */ "../views/About.vue"),
-  },
-  {
-    path: "/:pathMatch(.*)*",
-    name: "notFound",
-    component: PageNotFound,
-  },
+const routes = [{
+        path: "/",
+        name: "Home",
+        component: Home,
+    },
+    {
+        path: "/alerts",
+        name: "alerts",
+        component: Alerts,
+    },
+    {
+        path: "/buttons",
+        name: "buttons",
+        component: Button,
+    },
+    {
+        path: "/breadcrumbs",
+        name: "breadcrumbs",
+        component: Breadcrumbs,
+    },
+    {
+        path: "/commerce",
+        name: "commerce",
+        component: Commerce,
+    },
+    {
+        path: "/content",
+        name: "content",
+        component: Content,
+    },
+    {
+        path: "/cta",
+        name: "cta",
+        component: Cta,
+    },
+    {
+        path: "/footer",
+        name: "footer",
+        component: Footer,
+    },
+    {
+        path: "/blog",
+        name: "blog",
+        component: Blog,
+    },
+    {
+        path: "/navbars",
+        name: "navbars",
+        component: Navbar,
+    },
+    {
+        path: "/pagination",
+        name: "pagination",
+        component: Pagination,
+    },
+    {
+        path: "/tables",
+        name: "tables",
+        component: Table,
+    },
+    {
+        path: "/about",
+        name: "About",
+        // route level code-splitting
+        // this generates a separate chunk (about.[hash].js) for this route
+        // which is lazy-loaded when the route is visited.
+        component: () =>
+            import ( /* webpackChunkName: "about" */ "../views/About.vue"),
+    },
+    {
+        path: "/:pathMatch(.*)*",
+        name: "notFound",
+        component: PageNotFound,
+    },
 ];
 
-function restrict() {
-  return process.env.NODE_ENV === "development" ? "/creator" : "/";
-}
-
 const router = createRouter({
-  history: createWebHistory(process.env.BASE_URL),
-  routes,
+    history: createWebHistory(process.env.BASE_URL),
+    routes,
 });
 
 export default router;
