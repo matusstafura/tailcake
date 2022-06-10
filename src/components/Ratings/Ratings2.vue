64559ecbfa4bd06814efbca11a6d48375aea471c
Matus Stafura <kobe21@gmail.com>
2022-06-10 08:08:07 +0200

    add rating; improvements

--- src/components/Ratings/Ratings2.vue
+++ src/components/Ratings/Ratings2.vue
@@ -6,76 +6,79 @@
 import Element from "../Element.vue";
 export default {
   components: { Element },
-  name: "With count",
+  name: "With count and progress bar",
   data() {
     return {
       code: `
 <div class="max-w-xl">
   <div class="flex items-center">
-    <svg fill="#ffa41d" class="h-3" xmlns="http://www.w3.org/2000/svg" width="12" viewBox="0 0 24 24"><path d="M12 .587l3.668 7.568 8.332 1.151-6.064 5.828 1.48 8.279-7.416-3.967-7.417 3.967 1.481-8.279-6.064-5.828 8.332-1.151z" /></svg>
-    <svg fill="#ffa41d" class="h-3" xmlns="http://www.w3.org/2000/svg" width="12" viewBox="0 0 24 24"><path d="M12 .587l3.668 7.568 8.332 1.151-6.064 5.828 1.48 8.279-7.416-3.967-7.417 3.967 1.481-8.279-6.064-5.828 8.332-1.151z" /></svg>
-    <svg fill="#ffa41d" class="h-3" xmlns="http://www.w3.org/2000/svg" width="12" viewBox="0 0 24 24"><path d="M12 .587l3.668 7.568 8.332 1.151-6.064 5.828 1.48 8.279-7.416-3.967-7.417 3.967 1.481-8.279-6.064-5.828 8.332-1.151z" /></svg>
-    <svg fill="#ffa41d" class="h-3" xmlns="http://www.w3.org/2000/svg" width="12" viewBox="0 0 24 24"><path d="M12 .587l3.668 7.568 8.332 1.151-6.064 5.828 1.48 8.279-7.416-3.967-7.417 3.967 1.481-8.279-6.064-5.828 8.332-1.151z" /></svg>
-    <svg fill="#ffa41d" class="h-3" xmlns="http://www.w3.org/2000/svg" width="12" viewBox="0 0 24 24"><path d="M12 .587l3.668 7.568 8.332 1.151-6.064 5.828 1.48 8.279-7.416-3.967-7.417 3.967 1.481-8.279-6.064-5.828 8.332-1.151z" /></svg>
+    <svg fill="#ffa41d" class="w-5" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path d="M12 .288l2.833 8.718h9.167l-7.417 5.389 2.833 8.718-7.416-5.388-7.417 5.388 2.833-8.718-7.416-5.389h9.167z"/></svg>
+    <svg fill="#ffa41d" class="w-5" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path d="M12 .288l2.833 8.718h9.167l-7.417 5.389 2.833 8.718-7.416-5.388-7.417 5.388 2.833-8.718-7.416-5.389h9.167z"/></svg>
+    <svg fill="#ffa41d" class="w-5" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path d="M12 .288l2.833 8.718h9.167l-7.417 5.389 2.833 8.718-7.416-5.388-7.417 5.388 2.833-8.718-7.416-5.389h9.167z"/></svg>
+    <svg fill="#ffa41d" class="w-5" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path d="M12 .288l2.833 8.718h9.167l-7.417 5.389 2.833 8.718-7.416-5.388-7.417 5.388 2.833-8.718-7.416-5.389h9.167z"/></svg>
+    <svg fill="#ffa41d" class="w-5" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path d="M12 6.76l1.379 4.246h4.465l-3.612 2.625 1.379 4.246-3.611-2.625-3.612 2.625 1.379-4.246-3.612-2.625h4.465l1.38-4.246zm0-6.472l-2.833 8.718h-9.167l7.416 5.389-2.833 8.718 7.417-5.388 7.416 5.388-2.833-8.718 7.417-5.389h-9.167l-2.833-8.718z"/></svg>
+    
     <div class="relative w-64 mx-4 -mt-1">
-      <div class="absolute z-10 h-1 w-[100%] bg-gray-300"></div>
-      <div class="absolute z-20 h-1 w-[100%] bg-yellow-300"></div>
+      <div class="absolute z-10 h- w-[100%] bg-gray-300"></div>
+      <div class="absolute z-20 h-1 w-[100%] bg-[#ffa41d]"></div>
     </div>
-    <span class="text-[0.8rem] text-gray-600">95% (21)</span>
+    <span class="text-[0.8rem] text-gray-600 dark:text-gray-200">95% (21)</span>
   </div>
 
-  <div class="flex items-center">
-    <svg fill="#ffa41d" class="h-3" xmlns="http://www.w3.org/2000/svg" width="12" viewBox="0 0 24 24"><path d="M12 .587l3.668 7.568 8.332 1.151-6.064 5.828 1.48 8.279-7.416-3.967-7.417 3.967 1.481-8.279-6.064-5.828 8.332-1.151z" /></svg>
-    <svg fill="#ffa41d" class="h-3" xmlns="http://www.w3.org/2000/svg" width="12" viewBox="0 0 24 24"><path d="M12 .587l3.668 7.568 8.332 1.151-6.064 5.828 1.48 8.279-7.416-3.967-7.417 3.967 1.481-8.279-6.064-5.828 8.332-1.151z" /></svg>
-    <svg fill="#ffa41d" class="h-3" xmlns="http://www.w3.org/2000/svg" width="12" viewBox="0 0 24 24"><path d="M12 .587l3.668 7.568 8.332 1.151-6.064 5.828 1.48 8.279-7.416-3.967-7.417 3.967 1.481-8.279-6.064-5.828 8.332-1.151z" /></svg>
-    <svg fill="#ffa41d" class="h-3" xmlns="http://www.w3.org/2000/svg" width="12" viewBox="0 0 24 24"><path d="M12 .587l3.668 7.568 8.332 1.151-6.064 5.828 1.48 8.279-7.416-3.967-7.417 3.967 1.481-8.279-6.064-5.828 8.332-1.151z" /></svg>
-    <svg fill="#edeef1" class="h-3" xmlns="http://www.w3.org/2000/svg" width="12" viewBox="0 0 24 24"><path d="M12 .587l3.668 7.568 8.332 1.151-6.064 5.828 1.48 8.279-7.416-3.967-7.417 3.967 1.481-8.279-6.064-5.828 8.332-1.151z" /></svg>
+  <div class="flex items-center ">
+    <svg fill="#ffa41d" class="w-5" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path d="M12 .288l2.833 8.718h9.167l-7.417 5.389 2.833 8.718-7.416-5.388-7.417 5.388 2.833-8.718-7.416-5.389h9.167z"/></svg>
+    <svg fill="#ffa41d" class="w-5" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path d="M12 .288l2.833 8.718h9.167l-7.417 5.389 2.833 8.718-7.416-5.388-7.417 5.388 2.833-8.718-7.416-5.389h9.167z"/></svg>
+    <svg fill="#ffa41d" class="w-5" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path d="M12 .288l2.833 8.718h9.167l-7.417 5.389 2.833 8.718-7.416-5.388-7.417 5.388 2.833-8.718-7.416-5.389h9.167z"/></svg>
+    <svg fill="#ffa41d" class="w-5" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path d="M12 6.76l1.379 4.246h4.465l-3.612 2.625 1.379 4.246-3.611-2.625-3.612 2.625 1.379-4.246-3.612-2.625h4.465l1.38-4.246zm0-6.472l-2.833 8.718h-9.167l7.416 5.389-2.833 8.718 7.417-5.388 7.416 5.388-2.833-8.718 7.417-5.389h-9.167l-2.833-8.718z"/></svg>
+    <svg fill="#ffa41d" class="w-5" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path d="M12 6.76l1.379 4.246h4.465l-3.612 2.625 1.379 4.246-3.611-2.625-3.612 2.625 1.379-4.246-3.612-2.625h4.465l1.38-4.246zm0-6.472l-2.833 8.718h-9.167l7.416 5.389-2.833 8.718 7.417-5.388 7.416 5.388-2.833-8.718 7.417-5.389h-9.167l-2.833-8.718z"/></svg>
+
     <div class="relative w-64 mx-4 -mt-1">
       <div class="absolute z-10 h-1 w-[100%] bg-gray-300"></div>
-      <div class="absolute z-20 h-1 w-[80%] bg-yellow-300"></div>
+      <div class="absolute z-20 h-1 w-[80%] bg-[#ffa41d]"></div>
     </div>
-    <span class="text-[0.8rem] text-gray-600">77% (13)</span>
+    <span class="text-[0.8rem] text-gray-600 dark:text-gray-200">77% (13)</span>
   </div>
   <div class="flex items-center">
-    <svg fill="#ffa41d" class="h-3" xmlns="http://www.w3.org/2000/svg" width="12" viewBox="0 0 24 24"><path d="M12 .587l3.668 7.568 8.332 1.151-6.064 5.828 1.48 8.279-7.416-3.967-7.417 3.967 1.481-8.279-6.064-5.828 8.332-1.151z" /></svg>
-    <svg fill="#ffa41d" class="h-3" xmlns="http://www.w3.org/2000/svg" width="12" viewBox="0 0 24 24"><path d="M12 .587l3.668 7.568 8.332 1.151-6.064 5.828 1.48 8.279-7.416-3.967-7.417 3.967 1.481-8.279-6.064-5.828 8.332-1.151z" /></svg>
-    <svg fill="#ffa41d" class="h-3" xmlns="http://www.w3.org/2000/svg" width="12" viewBox="0 0 24 24"><path d="M12 .587l3.668 7.568 8.332 1.151-6.064 5.828 1.48 8.279-7.416-3.967-7.417 3.967 1.481-8.279-6.064-5.828 8.332-1.151z" /></svg>
-    <svg fill="#edeef1" class="h-3" xmlns="http://www.w3.org/2000/svg" width="12" viewBox="0 0 24 24"><path d="M12 .587l3.668 7.568 8.332 1.151-6.064 5.828 1.48 8.279-7.416-3.967-7.417 3.967 1.481-8.279-6.064-5.828 8.332-1.151z" /></svg>
-    <svg fill="#edeef1" class="h-3" xmlns="http://www.w3.org/2000/svg" width="12" viewBox="0 0 24 24"><path d="M12 .587l3.668 7.568 8.332 1.151-6.064 5.828 1.48 8.279-7.416-3.967-7.417 3.967 1.481-8.279-6.064-5.828 8.332-1.151z" /></svg>
+    <svg fill="#ffa41d" class="w-5" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path d="M12 .288l2.833 8.718h9.167l-7.417 5.389 2.833 8.718-7.416-5.388-7.417 5.388 2.833-8.718-7.416-5.389h9.167z"/></svg>
+    <svg fill="#ffa41d" class="w-5" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path d="M12 .288l2.833 8.718h9.167l-7.417 5.389 2.833 8.718-7.416-5.388-7.417 5.388 2.833-8.718-7.416-5.389h9.167z"/></svg>
+    <svg fill="#ffa41d" class="w-5" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path d="M12 6.76l1.379 4.246h4.465l-3.612 2.625 1.379 4.246-3.611-2.625-3.612 2.625 1.379-4.246-3.612-2.625h4.465l1.38-4.246zm0-6.472l-2.833 8.718h-9.167l7.416 5.389-2.833 8.718 7.417-5.388 7.416 5.388-2.833-8.718 7.417-5.389h-9.167l-2.833-8.718z"/></svg>
+    <svg fill="#ffa41d" class="w-5" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path d="M12 6.76l1.379 4.246h4.465l-3.612 2.625 1.379 4.246-3.611-2.625-3.612 2.625 1.379-4.246-3.612-2.625h4.465l1.38-4.246zm0-6.472l-2.833 8.718h-9.167l7.416 5.389-2.833 8.718 7.417-5.388 7.416 5.388-2.833-8.718 7.417-5.389h-9.167l-2.833-8.718z"/></svg>
+    <svg fill="#ffa41d" class="w-5" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path d="M12 6.76l1.379 4.246h4.465l-3.612 2.625 1.379 4.246-3.611-2.625-3.612 2.625 1.379-4.246-3.612-2.625h4.465l1.38-4.246zm0-6.472l-2.833 8.718h-9.167l7.416 5.389-2.833 8.718 7.417-5.388 7.416 5.388-2.833-8.718 7.417-5.389h-9.167l-2.833-8.718z"/></svg>
     <div class="relative w-64 mx-4 -mt-1">
       <div class="absolute z-10 h-1 w-[100%] bg-gray-300"></div>
-      <div class="absolute z-20 h-1 w-[60%] bg-yellow-300"></div>
+      <div class="absolute z-20 h-1 w-[60%] bg-[#ffa41d]"></div>
     </div>
-    <span class="text-[0.8rem] text-gray-600">65% (1)</span>
+    <span class="text-[0.8rem] text-gray-600 dark:text-gray-200">65% (1)</span>
   </div>
   <div class="flex items-center">
-    <svg fill="#ffa41d" class="h-3" xmlns="http://www.w3.org/2000/svg" width="12" viewBox="0 0 24 24"><path d="M12 .587l3.668 7.568 8.332 1.151-6.064 5.828 1.48 8.279-7.416-3.967-7.417 3.967 1.481-8.279-6.064-5.828 8.332-1.151z" /></svg>
-    <svg fill="#ffa41d" class="h-3" xmlns="http://www.w3.org/2000/svg" width="12" viewBox="0 0 24 24"><path d="M12 .587l3.668 7.568 8.332 1.151-6.064 5.828 1.48 8.279-7.416-3.967-7.417 3.967 1.481-8.279-6.064-5.828 8.332-1.151z" /></svg>
-    <svg fill="#edeef1" class="h-3" xmlns="http://www.w3.org/2000/svg" width="12" viewBox="0 0 24 24"><path d="M12 .587l3.668 7.568 8.332 1.151-6.064 5.828 1.48 8.279-7.416-3.967-7.417 3.967 1.481-8.279-6.064-5.828 8.332-1.151z" /></svg>
-    <svg fill="#edeef1" class="h-3" xmlns="http://www.w3.org/2000/svg" width="12" viewBox="0 0 24 24"><path d="M12 .587l3.668 7.568 8.332 1.151-6.064 5.828 1.48 8.279-7.416-3.967-7.417 3.967 1.481-8.279-6.064-5.828 8.332-1.151z" /></svg>
-    <svg fill="#edeef1" class="h-3" xmlns="http://www.w3.org/2000/svg" width="12" viewBox="0 0 24 24"><path d="M12 .587l3.668 7.568 8.332 1.151-6.064 5.828 1.48 8.279-7.416-3.967-7.417 3.967 1.481-8.279-6.064-5.828 8.332-1.151z" /></svg>
-    <div class="relative w-64 mx-4 -mt-1">
+    <svg fill="#ffa41d" class="w-5" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path d="M12 .288l2.833 8.718h9.167l-7.417 5.389 2.833 8.718-7.416-5.388-7.417 5.388 2.833-8.718-7.416-5.389h9.167z"/></svg>
+    <svg fill="#ffa41d" class="w-5" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path d="M12 6.76l1.379 4.246h4.465l-3.612 2.625 1.379 4.246-3.611-2.625-3.612 2.625 1.379-4.246-3.612-2.625h4.465l1.38-4.246zm0-6.472l-2.833 8.718h-9.167l7.416 5.389-2.833 8.718 7.417-5.388 7.416 5.388-2.833-8.718 7.417-5.389h-9.167l-2.833-8.718z"/></svg>
+    <svg fill="#ffa41d" class="w-5" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path d="M12 6.76l1.379 4.246h4.465l-3.612 2.625 1.379 4.246-3.611-2.625-3.612 2.625 1.379-4.246-3.612-2.625h4.465l1.38-4.246zm0-6.472l-2.833 8.718h-9.167l7.416 5.389-2.833 8.718 7.417-5.388 7.416 5.388-2.833-8.718 7.417-5.389h-9.167l-2.833-8.718z"/></svg>
+    <svg fill="#ffa41d" class="w-5" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path d="M12 6.76l1.379 4.246h4.465l-3.612 2.625 1.379 4.246-3.611-2.625-3.612 2.625 1.379-4.246-3.612-2.625h4.465l1.38-4.246zm0-6.472l-2.833 8.718h-9.167l7.416 5.389-2.833 8.718 7.417-5.388 7.416 5.388-2.833-8.718 7.417-5.389h-9.167l-2.833-8.718z"/></svg>
+    <svg fill="#ffa41d" class="w-5" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path d="M12 6.76l1.379 4.246h4.465l-3.612 2.625 1.379 4.246-3.611-2.625-3.612 2.625 1.379-4.246-3.612-2.625h4.465l1.38-4.246zm0-6.472l-2.833 8.718h-9.167l7.416 5.389-2.833 8.718 7.417-5.388 7.416 5.388-2.833-8.718 7.417-5.389h-9.167l-2.833-8.718z"/></svg>
+  <div class="relative w-64 mx-4 -mt-1">
       <div class="absolute z-10 h-1 w-[100%] bg-gray-300"></div>
-      <div class="absolute z-20 h-1 w-[40%] bg-yellow-300"></div>
+      <div class="absolute z-20 h-1 w-[40%] bg-[#ffa41d]"></div>
     </div>
-    <span class="text-[0.8rem] text-gray-600">45% (11)</span>
+    <span class="text-[0.8rem] text-gray-600 dark:text-gray-200">45% (11)</span>
   </div>
   <div class="flex items-center">
-    <svg fill="#ffa41d" class="h-3" xmlns="http://www.w3.org/2000/svg" width="12" viewBox="0 0 24 24"><path d="M12 .587l3.668 7.568 8.332 1.151-6.064 5.828 1.48 8.279-7.416-3.967-7.417 3.967 1.481-8.279-6.064-5.828 8.332-1.151z" /></svg>
-    <svg fill="#edeef1" class="h-3" xmlns="http://www.w3.org/2000/svg" width="12" viewBox="0 0 24 24"><path d="M12 .587l3.668 7.568 8.332 1.151-6.064 5.828 1.48 8.279-7.416-3.967-7.417 3.967 1.481-8.279-6.064-5.828 8.332-1.151z" /></svg>
-    <svg fill="#edeef1" class="h-3" xmlns="http://www.w3.org/2000/svg" width="12" viewBox="0 0 24 24"><path d="M12 .587l3.668 7.568 8.332 1.151-6.064 5.828 1.48 8.279-7.416-3.967-7.417 3.967 1.481-8.279-6.064-5.828 8.332-1.151z" /></svg>
-    <svg fill="#edeef1" class="h-3" xmlns="http://www.w3.org/2000/svg" width="12" viewBox="0 0 24 24"><path d="M12 .587l3.668 7.568 8.332 1.151-6.064 5.828 1.48 8.279-7.416-3.967-7.417 3.967 1.481-8.279-6.064-5.828 8.332-1.151z" /></svg>
-    <svg fill="#edeef1" class="h-3" xmlns="http://www.w3.org/2000/svg" width="12" viewBox="0 0 24 24"><path d="M12 .587l3.668 7.568 8.332 1.151-6.064 5.828 1.48 8.279-7.416-3.967-7.417 3.967 1.481-8.279-6.064-5.828 8.332-1.151z" /></svg>
-    <div class="relative w-64 mx-4 -mt-1">
+    <svg fill="#ffa41d" class="w-5" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path d="M12 6.76l1.379 4.246h4.465l-3.612 2.625 1.379 4.246-3.611-2.625-3.612 2.625 1.379-4.246-3.612-2.625h4.465l1.38-4.246zm0-6.472l-2.833 8.718h-9.167l7.416 5.389-2.833 8.718 7.417-5.388 7.416 5.388-2.833-8.718 7.417-5.389h-9.167l-2.833-8.718z"/></svg>
+    <svg fill="#ffa41d" class="w-5" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path d="M12 6.76l1.379 4.246h4.465l-3.612 2.625 1.379 4.246-3.611-2.625-3.612 2.625 1.379-4.246-3.612-2.625h4.465l1.38-4.246zm0-6.472l-2.833 8.718h-9.167l7.416 5.389-2.833 8.718 7.417-5.388 7.416 5.388-2.833-8.718 7.417-5.389h-9.167l-2.833-8.718z"/></svg>
+    <svg fill="#ffa41d" class="w-5" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path d="M12 6.76l1.379 4.246h4.465l-3.612 2.625 1.379 4.246-3.611-2.625-3.612 2.625 1.379-4.246-3.612-2.625h4.465l1.38-4.246zm0-6.472l-2.833 8.718h-9.167l7.416 5.389-2.833 8.718 7.417-5.388 7.416 5.388-2.833-8.718 7.417-5.389h-9.167l-2.833-8.718z"/></svg>
+    <svg fill="#ffa41d" class="w-5" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path d="M12 6.76l1.379 4.246h4.465l-3.612 2.625 1.379 4.246-3.611-2.625-3.612 2.625 1.379-4.246-3.612-2.625h4.465l1.38-4.246zm0-6.472l-2.833 8.718h-9.167l7.416 5.389-2.833 8.718 7.417-5.388 7.416 5.388-2.833-8.718 7.417-5.389h-9.167l-2.833-8.718z"/></svg>
+    <svg fill="#ffa41d" class="w-5" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path d="M12 6.76l1.379 4.246h4.465l-3.612 2.625 1.379 4.246-3.611-2.625-3.612 2.625 1.379-4.246-3.612-2.625h4.465l1.38-4.246zm0-6.472l-2.833 8.718h-9.167l7.416 5.389-2.833 8.718 7.417-5.388 7.416 5.388-2.833-8.718 7.417-5.389h-9.167l-2.833-8.718z"/></svg>
+  <div class="relative w-64 mx-4 -mt-1">
       <div class="absolute z-10 h-1 w-[100%] bg-gray-300"></div>
-      <div class="absolute z-20 h-1 w-[20%] bg-yellow-300"></div>
+      <div class="absolute z-20 h-1 w-[20%] bg-[#ffa41d]"></div>
     </div>
-    <span class="text-[0.8rem] text-gray-600">12% (1)</span>
+    <span class="text-[0.8rem] text-gray-600 dark:text-gray-200">12% (1)</span>
   </div>
 </div>
 
+
 `,
-      title: "With count",
+      title: "With count and progress bar",
     };
   },
 };
---
+++ src/components/Ratings/Ratings3.vue
@@ -0,0 +1,85 @@
+<template>
+  <Element :title="title" :code="code"></Element>
+</template>
+
+<script>
+import Element from "../Element.vue";
+export default {
+  components: { Element },
+  name: "With count and progress bar",
+  data() {
+    return {
+      code: `
+<div>
+  <div class="flex">
+    <svg fill="#ffa41d" class="h-5" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path d="M12 .587l3.668 7.568 8.332 1.151-6.064 5.828 1.48 8.279-7.416-3.967-7.417 3.967 1.481-8.279-6.064-5.828 8.332-1.151z" /></svg>
+    <svg fill="#ffa41d" class="h-5" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path d="M12 .587l3.668 7.568 8.332 1.151-6.064 5.828 1.48 8.279-7.416-3.967-7.417 3.967 1.481-8.279-6.064-5.828 8.332-1.151z" /></svg>
+    <svg fill="#ffa41d" class="h-5" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path d="M12 .587l3.668 7.568 8.332 1.151-6.064 5.828 1.48 8.279-7.416-3.967-7.417 3.967 1.481-8.279-6.064-5.828 8.332-1.151z" /></svg>
+    <svg fill="#ffa41d" class="h-5" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path d="M12 .587l3.668 7.568 8.332 1.151-6.064 5.828 1.48 8.279-7.416-3.967-7.417 3.967 1.481-8.279-6.064-5.828 8.332-1.151z" /></svg>
+    <svg fill="#edeef1" class="h-5" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path d="M12 .587l3.668 7.568 8.332 1.151-6.064 5.828 1.48 8.279-7.416-3.967-7.417 3.967 1.481-8.279-6.064-5.828 8.332-1.151z" /></svg>
+    <span class="ml-3 font-semibold dark:text-gray-100">4.1 out of 5</span>
+  </div>
+  <p class="py-3 text-slate-500 dark:text-gray-200">321 reviews</p>
+  <ul>
+    <li class="flex mb-2">
+      <span class="dark:text-gray-200">5 star</span>
+      <div class="relative w-64 mx-4">
+        <div class="absolute z-10 h-5 w-[100%] rounded border border-gray-200 bg-gray-100"></div>
+        <div class="absolute z-20 h-5 w-[84%] rounded-l border border-yellow-500 bg-[#ffa41d]"></div>
+      </div>
+      <span class="-mt-1">
+        <a href="/reviews" class="text-blue-600 hover:underline dark:text-blue-300">84%</a>
+      </span>
+    </li>
+    <li class="flex mb-2">
+      <span class="dark:text-gray-200">4 star</span>
+      <div class="relative w-64 mx-4">
+        <div class="absolute z-10 h-5 w-[100%] rounded border border-gray-200 bg-gray-100"></div>
+        <div class="absolute z-20 h-5 w-[69%] rounded-l border border-yellow-500 bg-[#ffa41d]"></div>
+      </div>
+      <span class="-mt-1">
+        <a href="/reviews" class="text-blue-600 hover:underline dark:text-blue-300">69%</a>
+      </span>
+    </li>
+    <li class="flex mb-2">
+      <span class="dark:text-gray-200">3 star</span>
+      <div class="relative w-64 mx-4">
+        <div class="absolute z-10 h-5 w-[100%] rounded border border-gray-200 bg-gray-100"></div>
+        <div class="absolute z-20 h-5 w-[51%] rounded-l border border-yellow-500 bg-[#ffa41d]"></div>
+      </div>
+      <span class="-mt-1">
+        <a href="/reviews" class="text-blue-600 hover:underline dark:text-blue-300">51%</a>
+      </span>
+    </li>
+    <li class="flex mb-2">
+      <span class="dark:text-gray-200">2 star</span>
+      <div class="relative w-64 mx-4">
+        <div class="absolute z-10 h-5 w-[100%] rounded border border-gray-200 bg-gray-100"></div>
+        <div class="absolute z-20 h-5 w-[36%] rounded-l border border-yellow-500 bg-[#ffa41d]"></div>
+      </div>
+      <span class="-mt-1">
+        <a href="/reviews" class="text-blue-600 hover:underline dark:text-blue-300">36%</a>
+      </span>
+    </li>
+    <li class="flex mb-2">
+      <span class="dark:text-gray-200">1 star</span>
+      <div class="relative w-64 mx-4">
+        <div class="absolute z-10 h-5 w-[100%] rounded border border-gray-200 bg-gray-100"></div>
+        <div class="absolute z-20 h-5 w-[11%] rounded-l border border-yellow-500 bg-[#ffa41d]"></div>
+      </div>
+      <span class="-mt-1">
+        <a href="/reviews" class="text-blue-600 hover:underline dark:text-blue-300">11%</a>
+      </span>
+    </li>
+  </ul>
+</div>
+
+
+`,
+      title: "With count and progress bar",
+    };
+  },
+};
+</script>
+
+<style scoped></style>
