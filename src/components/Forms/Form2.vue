2edd1c90c9e34120ee112208fc469e455d491c13
Matus Stafura <kobe21@gmail.com>
2022-06-16 09:54:46 +0200

    add form, table

---
+++ src/components/Forms/Form2.vue
@@ -0,0 +1,42 @@
+<template>
+  <Element :title="title" :code="code"></Element>
+</template>
+
+<script>
+import Element from "../Element.vue";
+export default {
+  components: { Element },
+  name: "Sign up",
+  data() {
+    return {
+      code: `
+<form>
+  <div class="grid max-w-md gap-y-4 p-4">
+    <p class="text-xl font-semibold">Sign in to your account</p>
+    <div>
+      <label for="email" class="mb-2 block text-sm font-semibold">Email</label>
+      <input type="text" placeholder="my@email.com" class="w-full rounded-lg border border-gray-200 bg-slate-50 p-2" />
+    </div>
+    <div>
+      <div class="flex justify-between">
+        <label for="password" class="mb-2 text-sm font-semibold">Password</label>
+        <a href="/forgot-password" class="text-sm text-blue-500">Forgot your password?</a>
+      </div>
+      <input type="password" placeholder="password" class="w-full rounded-lg border border-gray-200 bg-slate-50 p-2" />
+    </div>
+    <div class="flex items-center py-3">
+      <input checked id="remember-me" type="checkbox" value="" class="h-4 w-4 rounded border-gray-300 bg-gray-100 text-blue-500" />
+      <label for="remember-me" class="ml-2 text-sm">Stay signed for a week</label>
+    </div>
+    <button class="rounded bg-blue-400 px-4 py-2 text-white shadow-md hover:bg-blue-500 dark:hover:bg-blue-500 dark:hover:text-gray-100">Sign up</button>
+  </div>
+</form>
+
+`,
+      title: "Sign up",
+    };
+  },
+};
+</script>
+
+<style scoped></style>
---
+++ src/components/Tables/Table3.vue
@@ -0,0 +1,64 @@
+<template>
+  <Element :title="title" :code="code"></Element>
+</template>
+
+<script>
+import Element from "../Element.vue";
+export default {
+  components: { Element },
+  name: "Product Table",
+  data() {
+    return {
+      code: `
+<div>
+  <p class="text-2xl font-bold">Payments</p>
+  <table class="w-full text-left">
+    <thead class="text-sm uppercase text-gray-700">
+      <tr class="border-b">
+        <th class="py-2">All</th>
+        <th class="py-2">Name</th>
+        <th class="py-2">email</th>
+        <th class="py-2">
+          <div class="flex">
+            <span>Total</span>
+            <button type="submit">
+              <svg class="h-5 fill-gray-400 hover:fill-slate-300" clip-rule="evenodd" fill-rule="evenodd" stroke-linejoin="round" stroke-miterlimit="2" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path d="m5.214 14.522s4.505 4.502 6.259 6.255c.146.147.338.22.53.22s.384-.073.53-.22c1.754-1.752 6.249-6.244 6.249-6.244.144-.144.216-.334.217-.523 0-.193-.074-.386-.221-.534-.293-.293-.766-.294-1.057-.004l-4.968 4.968v-14.692c0-.414-.336-.75-.75-.75s-.75.336-.75.75v14.692l-4.979-4.978c-.289-.289-.761-.287-1.054.006-.148.148-.222.341-.221.534 0 .189.071.377.215.52z" fill-rule="nonzero" /></svg>
+            </button>
+            <button type="submit">
+              <svg class="h-5 fill-gray-400 hover:fill-slate-300" clip-rule="evenodd" fill-rule="evenodd" stroke-linejoin="round" stroke-miterlimit="2" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path d="m18.787 9.473s-4.505-4.502-6.259-6.255c-.147-.146-.339-.22-.53-.22-.192 0-.384.074-.531.22-1.753 1.753-6.256 6.252-6.256 6.252-.147.147-.219.339-.217.532.001.19.075.38.221.525.292.293.766.295 1.056.004l4.977-4.976v14.692c0 .414.336.75.75.75.413 0 .75-.336.75-.75v-14.692l4.978 4.978c.289.29.762.287 1.055-.006.145-.145.219-.335.221-.525.002-.192-.07-.384-.215-.529z" fill-rule="nonzero" /></svg>
+            </button>
+          </div>
+        </th>
+      </tr>
+    </thead>
+    <tbody>
+      <tr class="border-b">
+        <td>
+          <div class="flex items-center">
+            <input id="checkbox-table-1" type="checkbox" class="h-4 w-4 rounded border-gray-300 bg-gray-100 text-blue-600" />
+            <label for="checkbox-table-1" class="sr-only">checkbox</label>
+          </div>
+        </td>
+        <td class="py-2">James L. Coombs</td>
+        <td class="py-2 font-light text-gray-500">jameslcoombs@example.com</td>
+        <td class="py-2">USD33.44</td>
+      </tr>
+    </tbody>
+  </table>
+  <div class="mt-4 flex justify-between py-2 text-sm text-gray-500">
+    <span>123 results</span>
+    <div>
+      <a href="/previous" class="ml-2 rounded border p-2 shadow">Previous</a>
+      <a href="/next" class="ml-2 rounded border p-2 text-gray-800 shadow">Next</a>
+    </div>
+  </div>
+</div>
+      
+      `,
+      title: "Product Table",
+    };
+  },
+};
+</script>
+
+<style scoped></style>
