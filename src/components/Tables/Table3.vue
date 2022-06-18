ecb74e562b3f3bc84f509b9428f951ffc8d5e085
Matus Stafura <kobe21@gmail.com>
2022-06-18 09:45:49 +0200

    add table

--- src/components/Tables/Table3.vue
+++ src/components/Tables/Table3.vue
@@ -29,10 +29,18 @@ export default {
             </button>
           </div>
         </th>
+        <th class="py-2">
+          <div class="flex">
+            <span>Last Payment</span>
+            <button type="submit">
+              <svg class="h-5 fill-gray-400 hover:fill-slate-300" clip-rule="evenodd" fill-rule="evenodd" stroke-linejoin="round" stroke-miterlimit="2" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path d="m5.214 14.522s4.505 4.502 6.259 6.255c.146.147.338.22.53.22s.384-.073.53-.22c1.754-1.752 6.249-6.244 6.249-6.244.144-.144.216-.334.217-.523 0-.193-.074-.386-.221-.534-.293-.293-.766-.294-1.057-.004l-4.968 4.968v-14.692c0-.414-.336-.75-.75-.75s-.75.336-.75.75v14.692l-4.979-4.978c-.289-.289-.761-.287-1.054.006-.148.148-.222.341-.221.534 0 .189.071.377.215.52z" fill-rule="nonzero" /></svg>
+            </button>
+          </div>
+        </th>
       </tr>
     </thead>
     <tbody class="dark:text-gray-300">
-      <tr class="border-b">
+      <tr class="text-gray-600 border-b dark:text-gray-300">
         <td>
           <div class="flex items-center">
             <input id="checkbox-table-1" type="checkbox" class="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 rounded" />
@@ -41,7 +49,20 @@ export default {
         </td>
         <td class="py-2">James L. Coombs</td>
         <td class="py-2 font-light text-gray-500 dark:text-gray-300">jameslcoombs@example.com</td>
-        <td class="py-2">USD33.44</td>
+        <td class="py-2">€33.44</td>
+        <td class="py-2">Jan 08, 2023</td>
+      </tr>
+      <tr class="text-gray-600 border-b dark:text-gray-300">
+        <td>
+          <div class="flex items-center">
+            <input id="checkbox-table-2" type="checkbox" class="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 rounded" />
+            <label for="checkbox-table-2" class="sr-only">checkbox</label>
+          </div>
+        </td>
+        <td class="py-2">Peter Smith</td>
+        <td class="py-2 font-light text-gray-500 dark:text-gray-300">psmith@example.com</td>
+        <td class="py-2">€12.23</td>
+        <td class="py-2">Jan 02, 2023</td>
       </tr>
     </tbody>
   </table>
@@ -53,7 +74,7 @@ export default {
     </div>
   </div>
 </div>
-      
+
       `,
       title: "Product Table",
     };
---
+++ src/components/Tables/Table4.vue
@@ -0,0 +1,67 @@
+<template>
+  <Element :title="title" :code="code"></Element>
+</template>
+
+<script>
+import Element from "../Element.vue";
+export default {
+  components: { Element },
+  name: "Product Table",
+  data() {
+    return {
+      code: `
+<div>
+  <p class="text-lg font-bold dark:text-gray-300">Report</p>
+  <p class="py-2 text-sm text-gray-500 dark:text-gray-300">Jan 1, 2022 - Jan 31, 2022</p>
+  <table class="w-full text-left text-[0.95rem]">
+    <thead class="text-sm text-gray-700 dark:text-gray-300">
+      <tr class="border-b">
+        <th class="py-4">Name</th>
+        <th class="py-4 text-right">Quantity</th>
+        <th class="py-4 text-right">Price</th>
+        <th class="py-4 text-right">Sum Total</th>
+      </tr>
+    </thead>
+    <tbody>
+      <tr class="text-gray-500 border-b dark:text-gray-300">
+        <td class="py-4 text-black dark:text-gray-300">James L. Coombs</td>
+        <td class="py-4 font-light text-right text-gray-500 dark:text-gray-300">33</td>
+        <td class="py-4 text-right">€33.44</td>
+        <td class="py-4 text-right">€1,234.55</td>
+      </tr>
+      <tr class="text-gray-500 border-b dark:text-gray-300">
+        <td class="py-4 text-black dark:text-gray-300">Peter Smith</td>
+        <td class="py-4 font-light text-right text-gray-500 dark:text-gray-300">13</td>
+        <td class="py-4 text-right">€12.23</td>
+        <td class="py-4 text-right">€4,324.73</td>
+      </tr>
+      <tr class="text-gray-500 dark:text-gray-300">
+        <td class="py-4 text-black"></td>
+        <td class="py-4 font-light text-right text-gray-500 dark:text-gray-300"></td>
+        <td class="py-4 text-right">Subtotal</td>
+        <td class="py-4 text-right">€8,324.73</td>
+      </tr>
+      <tr class="text-gray-500 dark:text-gray-300">
+        <td class="py-4 text-black"></td>
+        <td class="py-4 font-light text-right text-gray-500 dark:text-gray-300"></td>
+        <td class="py-4 text-right">Tax (20%)</td>
+        <td class="py-4 text-right">€1,324.73</td>
+      </tr>
+      <tr class="font-bold text-gray-500 dark:text-gray-300">
+        <td class="py-4 text-black"></td>
+        <td class="py-4 font-light text-right text-gray-500 dark:text-gray-300"></td>
+        <td class="py-4 text-right">Total</td>
+        <td class="text-right py-k">€1,324.73</td>
+      </tr>
+    </tbody>
+  </table>
+</div>
+
+      `,
+      title: "Product Table",
+    };
+  },
+};
+</script>
+
+<style scoped></style>
