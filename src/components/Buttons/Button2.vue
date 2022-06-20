9f5748f82fef04e3e8b166302a751c0fc65179ba
Matus Stafura <kobe21@gmail.com>
2022-06-20 09:27:40 +0200

    add product overview, update icon button

--- src/components/Buttons/Button2.vue
+++ src/components/Buttons/Button2.vue
@@ -10,9 +10,9 @@ export default {
   data() {
     return {
       code: `
-<button class="flex px-4 py-2 m-2 text-white bg-blue-400 rounded shadow-md hover:bg-blue-500 dark:hover:bg-blue-500 dark:hover:text-gray-100">
-  <svg class="w-4 mr-2" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="#ffffff"><path d="M4.558 7l4.701-4.702c.199-.198.46-.298.721-.298.613 0 1.02.505 1.02 1.029 0 .25-.092.504-.299.711l-3.26 3.26h-2.883zm12.001 0h2.883l-4.701-4.702c-.199-.198-.46-.298-.721-.298-.613 0-1.02.505-1.02 1.029 0 .25.092.504.299.711l3.26 3.26zm3.703 4l-.016.041-3.598 8.959h-9.296l-3.597-8.961-.016-.039h16.523zm3.738-2h-24v2h.643c.534 0 1.021.304 1.256.784l4.101 10.216h12l4.102-10.214c.233-.481.722-.786 1.256-.786h.642v-2z" /></svg>
-  Add to cart
+<button class="flex px-8 py-3 text-white bg-blue-400 rounded shadow-md hover:bg-blue-500 dark:hover:bg-blue-500 dark:hover:text-gray-100">
+  <svg class="w-5 mr-4" fill="#ffffff" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path d="M16 6v-2c0-2.209-1.791-4-4-4s-4 1.791-4 4v2h-5v18h18v-18h-5zm-7-2c0-1.654 1.346-3 3-3s3 1.346 3 3v2h-6v-2zm10 18h-14v-14h3v1.5c0 .276.224.5.5.5s.5-.224.5-.5v-1.5h6v1.5c0 .276.224.5.5.5s.5-.224.5-.5v-1.5h3v14z" /></svg>
+  <span class="text-lg">Add to Cart</span>
 </button>
 `,
       title: "Commerce",
---
+++ src/components/Commerce/Commerce4.vue
@@ -0,0 +1,71 @@
+<template>
+  <Element :title="title" :code="code"></Element>
+</template>
+
+<script>
+import Element from "../Element.vue";
+export default {
+  components: { Element },
+  name: "Product Overview",
+  data() {
+    return {
+      code: `
+<div>
+  <div class="grid grid-cols-1 gap-x-20 lg:grid-cols-2">
+    <div class="max-w-2xl"><img src="https://tailcake.netlify.app/images/commerce/a2.jpg" alt="" class="rounded-xl" /></div>
+    <div class="max-w-2xl">
+      <h1 class="text-4xl font-semibold tracking-wide text-slate-700">Retro Camera</h1>
+      <p class="py-4 text-xl text-gray-500">$55.00</p>
+      <p class="py-4 text-gray-500">Vestibulum id ligula porta felis euismod semper. Nullam id dolor id nibh ultricies vehicula ut id elit. Maecenas faucibus mollis interdum. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus.</p>
+      <div class="mb-6 dark:text-white">
+        <p class="pb-4 mb-4 font-semibold text-gray-600 uppercase border-b">Select color</p>
+
+        <ul class="flex font-light leading-9 text-gray-600">
+          <li class="mr-2 bg-white rounded-full shadow"><a href="#" class="block w-8 h-8 rounded-full hover:shadow-lg"></a></li>
+          <li class="mr-2 bg-yellow-500 rounded-full shadow"><a href="#" class="block w-8 h-8 rounded-full hover:shadow-lg"></a></li>
+          <li class="mr-2 bg-red-500 rounded-full shadow"><a href="#" class="block w-8 h-8 rounded-full hover:shadow-lg"></a></li>
+          <li class="mr-2 bg-orange-600 rounded-full shadow"><a href="#" class="block w-8 h-8 rounded-full hover:shadow-lg"></a></li>
+          <li class="mr-2 bg-green-500 rounded-full shadow"><a href="#" class="block w-8 h-8 rounded-full hover:shadow-lg"></a></li>
+          <li class="mr-2 bg-blue-500 rounded-full shadow"><a href="#" class="block w-8 h-8 rounded-full hover:shadow-lg"></a></li>
+          <li class="mr-2 bg-gray-500 rounded-full shadow"><a href="#" class="block w-8 h-8 rounded-full hover:shadow-lg"></a></li>
+          <li class="mr-2 bg-black rounded-full shadow"><a href="#" class="block w-8 h-8 rounded-full hover:shadow-lg"></a></li>
+        </ul>
+      </div>
+      <!-- COLOR E -->
+      <!-- SIZE S -->
+      <div class="mb-6 dark:text-slate-200">
+        <p class="pb-4 mb-4 font-semibold text-gray-600 uppercase border-b">SIZE</p>
+
+        <ul class="flex font-light leading-9 text-gray-600 dark:text-slate-200">
+          <li class="mr-2 text-center bg-gray-100 rounded-full shadow hover:bg-blue-200 hover:text-blue-600 dark:bg-gray-400 dark:hover:bg-blue-300"><a href="#" class="block w-8">XS</a></li>
+          <li class="mr-2 text-center bg-gray-100 rounded-full shadow hover:bg-blue-200 hover:text-blue-600 dark:bg-gray-400 dark:hover:bg-blue-300"><a href="#" class="block w-8">S</a></li>
+          <li class="mr-2 text-center bg-gray-100 rounded-full shadow hover:bg-blue-200 hover:text-blue-600 dark:bg-gray-400 dark:hover:bg-blue-300"><a href="#" class="block w-8">M</a></li>
+          <li class="mr-2 text-center bg-gray-100 rounded-full shadow hover:bg-blue-200 hover:text-blue-600 dark:bg-gray-400 dark:hover:bg-blue-300"><a href="#" class="block w-8">L</a></li>
+          <li class="mr-2 text-center bg-gray-100 rounded-full shadow hover:bg-blue-200 hover:text-blue-600 dark:bg-gray-400 dark:hover:bg-blue-300"><a href="#" class="block w-8">XL</a></li>
+        </ul>
+      </div>
+      <!-- SIZE E -->
+
+      <div class="flex py-3">
+        <select name="qty" id="qty" class="w-16 px-4 mr-3 text-gray-500 border rounded-md">
+          <option value="1">1</option>
+          <option value="2">2</option>
+        </select>
+
+        <button class="flex items-center px-8 py-3 text-white bg-blue-400 rounded shadow-md hover:bg-blue-500 dark:hover:bg-blue-500 dark:hover:text-gray-100">
+          <svg class="w-5 mr-4" fill="#ffffff" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path d="M16 6v-2c0-2.209-1.791-4-4-4s-4 1.791-4 4v2h-5v18h18v-18h-5zm-7-2c0-1.654 1.346-3 3-3s3 1.346 3 3v2h-6v-2zm10 18h-14v-14h3v1.5c0 .276.224.5.5.5s.5-.224.5-.5v-1.5h6v1.5c0 .276.224.5.5.5s.5-.224.5-.5v-1.5h3v14z" /></svg>
+          <span class="text-lg">Add to cart</span>
+        </button>
+      </div>
+    </div>
+  </div>
+</div>
+
+`,
+      title: "Product Overview",
+    };
+  },
+};
+</script>
+
+<style scoped></style>
