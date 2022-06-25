d588693b44637c89f8fc885163362bcef6b29772
Matus Stafura <kobe21@gmail.com>
2022-06-25 10:03:14 +0200

    add faq and pricing

---
+++ src/components/Content/Content2.vue
@@ -0,0 +1,55 @@
+<template>
+  <Element :title="title" :code="code"></Element>
+</template>
+
+<script>
+import Element from "../Element.vue";
+export default {
+  components: { Element },
+  name: "FAQ vertical",
+  data() {
+    return {
+      code: `
+<div class="max-w-5xl mx-auto dark:text-gray-200">
+  <h2 class="py-4 text-2xl font-bold">Faqs</h2>
+  <ul>
+    <li class="py-4">
+      <div class="flex justify-between pb-4 border-b">
+        <h3 class="text-lg font-semibold">Can I cancel or change my order?</h3>
+        <button>
+          <svg class="dark:fill-gray-200 fill-gray-500" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path d="M12 2c5.514 0 10 4.486 10 10s-4.486 10-10 10-10-4.486-10-10 4.486-10 10-10zm0-2c-6.627 0-12 5.373-12 12s5.373 12 12 12 12-5.373 12-12-5.373-12-12-12zm5 15.538l-3.592-3.548 3.546-3.587-1.416-1.403-3.545 3.589-3.588-3.543-1.405 1.405 3.593 3.552-3.547 3.592 1.405 1.405 3.555-3.596 3.591 3.55 1.403-1.416z" /></svg>
+        </button>
+      </div>
+      <p class="py-4 text-gray-500 dark:text-gray-300">Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin literature Id pro doctus mediocrem erroribus, diam nostro sed cu. Ea pri graeco tritani partiendo. Omittantur No tale choro fastidii his, pri cu epicuri perpetua. Enim dictas omittantur et duo, vocent lucilius quaestio mea ex. Ex illum officiis id.</p>
+    </li>
+
+    <li class="py-4">
+      <div class="flex justify-between pb-4 border-b">
+        <h3 class="text-lg font-semibold">How can I return an item purchased online?</h3>
+        <button>
+          <svg class="dark:fill-gray-200 fill-gray-500" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path d="M12 2c5.514 0 10 4.486 10 10s-4.486 10-10 10-10-4.486-10-10 4.486-10 10-10zm0-2c-6.627 0-12 5.373-12 12s5.373 12 12 12 12-5.373 12-12-5.373-12-12-12zm6 13h-5v5h-2v-5h-5v-2h5v-5h2v5h5v2z" /></svg>
+        </button>
+      </div>
+      <p class="hidden py-4 text-gray-500 dark:text-gray-300">Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin literature Id pro doctus mediocrem erroribus, diam nostro sed cu. Ea pri graeco tritani partiendo. Omittantur No tale choro fastidii his, pri cu epicuri perpetua. Enim dictas omittantur et duo, vocent lucilius quaestio mea ex. Ex illum officiis id.</p>
+    </li>
+
+    <li class="py-4">
+      <div class="flex justify-between pb-4 border-b">
+        <h3 class="text-lg font-semibold">How can I pay for my purchases?</h3>
+        <button>
+          <svg class="dark:fill-gray-200 fill-gray-500" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path d="M12 2c5.514 0 10 4.486 10 10s-4.486 10-10 10-10-4.486-10-10 4.486-10 10-10zm0-2c-6.627 0-12 5.373-12 12s5.373 12 12 12 12-5.373 12-12-5.373-12-12-12zm6 13h-5v5h-2v-5h-5v-2h5v-5h2v5h5v2z" /></svg>
+        </button>
+      </div>
+      <p class="hidden py-4 text-gray-500 dark:text-gray-300">Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin literature Id pro doctus mediocrem erroribus, diam nostro sed cu. Ea pri graeco tritani partiendo. Omittantur No tale choro fastidii his, pri cu epicuri perpetua. Enim dictas omittantur et duo, vocent lucilius quaestio mea ex. Ex illum officiis id.</p>
+    </li>
+  </ul>
+</div>
+
+`,
+      title: "FAQ vertical",
+    };
+  },
+};
+</script>
+
+<style scoped></style>
---
+++ src/components/Pricing/Pricing3.vue
@@ -0,0 +1,63 @@
+<template>
+  <Element :title="title" :code="code"></Element>
+</template>
+
+<script>
+import Element from "../Element.vue";
+export default {
+  components: { Element },
+  name: "Pricing Block",
+  data() {
+    return {
+      code: `
+<div class="max-w-lg p-4 mx-auto md:max-w-4xl">
+  <div class="grid grid-cols-1 border border-gray-100 shadow rounded-xl md:grid-cols-3">
+    <div class="col-span-2 p-8">
+      <h3 class="pb-3 text-2xl font-semibold">Lifetime Membership</h3>
+      <p class="text-gray-500">Learn from like-minded individuals which are eager to make a living building stuff on the web. Pay once and get lifetime access to the community.</p>
+      <div class="relative py-4 text-gray-500 border-b">
+        <div class="absolute pr-4 mt-1 uppercase bg-white">Features</div>
+      </div>
+
+      <ul class="grid grid-cols-1 py-4 mt-4 text-gray-500 gap-y-4 md:grid-cols-2">
+        <li class="flex">
+          <svg class="p-1 mt-1 mr-2 bg-indigo-500 rounded-xl fill-indigo-100" xmlns="http://www.w3.org/2000/svg" width="18" height="18" viewBox="0 0 24 24"><path d="M20.285 2l-11.285 11.567-5.286-5.011-3.714 3.716 9 8.728 15-15.285z"></path></svg>
+          <span>Private forum access</span>
+        </li>
+        <li class="flex">
+          <svg class="p-1 mt-1 mr-2 bg-indigo-500 rounded-xl fill-indigo-100" xmlns="http://www.w3.org/2000/svg" width="18" height="18" viewBox="0 0 24 24"><path d="M20.285 2l-11.285 11.567-5.286-5.011-3.714 3.716 9 8.728 15-15.285z"></path></svg>
+          <span>Private forum access</span>
+        </li>
+        <li class="flex">
+          <svg class="p-1 mt-1 mr-2 bg-indigo-500 rounded-xl fill-indigo-100" xmlns="http://www.w3.org/2000/svg" width="18" height="18" viewBox="0 0 24 24"><path d="M20.285 2l-11.285 11.567-5.286-5.011-3.714 3.716 9 8.728 15-15.285z"></path></svg>
+          <span>Private forum access</span>
+        </li>
+        <li class="flex">
+          <svg class="p-1 mt-1 mr-2 bg-indigo-500 rounded-xl fill-indigo-100" xmlns="http://www.w3.org/2000/svg" width="18" height="18" viewBox="0 0 24 24"><path d="M20.285 2l-11.285 11.567-5.286-5.011-3.714 3.716 9 8.728 15-15.285z"></path></svg>
+          <span>Private forum access</span>
+        </li>
+      </ul>
+    </div>
+    <div class="flex flex-col p-8 text-center bg-slate-50">
+      <div class="m-auto">
+        <div class="mb-3 text-6xl font-extrabold">$599</div>
+        <p class="flex-grow pb-2 text-sm text-gray-500">No monthly subscription, you only pay once.</p>
+      </div>
+      <div>
+        <button type="submit" class="px-6 py-2 m-2 text-white bg-indigo-500 rounded-3xl hover:bg-black">
+          <span>Get in touch</span>
+          <svg fill="#ffffff" class="inline w-3 ml-2 -mt-1 text-white" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"><path d="M7.33 24l-2.83-2.829 9.339-9.175-9.339-9.167 2.83-2.829 12.17 11.996z" /></svg>
+        </button>
+      </div>
+    </div>
+  </div>
+</div>
+
+`,
+      title: "Pricing Block",
+    };
+  },
+};
+</script>
+
+<style scoped></style>
