0e22ddae8e9a56283a797561e9676eb88dad5aca
Matus Stafura <kobe21@gmail.com>
2022-07-01 09:39:29 +0200

    redesign sidebar

--- src/App.vue
+++ src/App.vue
@@ -1,38 +1,9 @@
 <template>
-  <div class="bg-gradient-to-r from-blue-50 to-teal-50 via-gray-50">
-    <div id="nav" class="flex justify-between px-8 py-8">
-      <div class="flex">
-        <a href="#" class="text-gray-400" @click="toggleMenu">
-          <svg
-            class="mr-4 lg:hidden"
-            fill="hsl(224, 7.2%, 40%)"
-            viewBox="0 0 100 80"
-            width="32"
-            height="32"
-          >
-            <rect width="80" height="14"></rect>
-            <rect y="30" width="80" height="14"></rect>
-            <rect y="60" width="80" height="14"></rect>
-          </svg>
-        </a>
-        <div class="flex">
-          <a href="/" class="flex">
-            <div class="mr-4">
-              <img src="tailcake-logo.png" class="w-8 h-8" />
-            </div>
-            <div class="m-auto text-xl font-semibold">TailCake</div>
-          </a>
-        </div>
-      </div>
+  <div class="flex py-4">
+    <Sidebar></Sidebar>
+    <div id="content" class="w-full px-8 pl-64">
       <Menu></Menu>
-    </div>
-    <div class="flex">
-      <div class="lg:block" :class="sidebar ? 'hidden' : 'block'">
-        <Sidebar></Sidebar>
-      </div>
-      <div id="content" class="w-full px-8 py-4">
-        <router-view></router-view>
-      </div>
+      <router-view></router-view>
     </div>
   </div>
 </template>
--- src/components/Commerce/Commerce2.vue
+++ src/components/Commerce/Commerce2.vue
@@ -10,37 +10,65 @@ export default {
   data() {
     return {
       code: `
-<div class="grid grid-cols-1 gap-x-8 gap-y-8 dark:text-white md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4">
+<div class="grid grid-cols-1 p-4 gap-x-8 gap-y-8 dark:text-white md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4">
   <div>
-    <img src="https://tailcake.netlify.app/images/commerce/3.jpg" />
-    <div class="mt-4">
-      <a href="#" class="hover:text-blue-500">Shirt</a>
-      <p class="text-sm text-gray-500 dark:text-gray-300">Navy Blue</p>
-      <p class="text-xl">1220kr</p>
+    <a href="#"><img src="https://tailcake.netlify.app/images/commerce/3.jpg" class="hover:shadow-xl" /></a>
+    <div class="mt-4 text-center">
+      <a href="#" class="text-lg text-slate-500 hover:text-blue-500">Shirt</a>
+
+      <div class="flex justify-center my-2">
+        <svg fill="#ffa41d" class="h-4" xmlns="http://www.w3.org/2000/svg" width="16" height="16" viewBox="0 0 24 24"><path d="M12 .587l3.668 7.568 8.332 1.151-6.064 5.828 1.48 8.279-7.416-3.967-7.417 3.967 1.481-8.279-6.064-5.828 8.332-1.151z" /></svg>
+        <svg fill="#ffa41d" class="h-4" xmlns="http://www.w3.org/2000/svg" width="16" height="16" viewBox="0 0 24 24"><path d="M12 .587l3.668 7.568 8.332 1.151-6.064 5.828 1.48 8.279-7.416-3.967-7.417 3.967 1.481-8.279-6.064-5.828 8.332-1.151z" /></svg>
+        <svg fill="#ffa41d" class="h-4" xmlns="http://www.w3.org/2000/svg" width="16" height="16" viewBox="0 0 24 24"><path d="M12 .587l3.668 7.568 8.332 1.151-6.064 5.828 1.48 8.279-7.416-3.967-7.417 3.967 1.481-8.279-6.064-5.828 8.332-1.151z" /></svg>
+        <svg fill="#ffa41d" class="h-4" xmlns="http://www.w3.org/2000/svg" width="16" height="16" viewBox="0 0 24 24"><path d="M12 .587l3.668 7.568 8.332 1.151-6.064 5.828 1.48 8.279-7.416-3.967-7.417 3.967 1.481-8.279-6.064-5.828 8.332-1.151z" /></svg>
+        <svg fill="#edeef1" class="h-4" xmlns="http://www.w3.org/2000/svg" width="16" height="16" viewBox="0 0 24 24"><path d="M12 .587l3.668 7.568 8.332 1.151-6.064 5.828 1.48 8.279-7.416-3.967-7.417 3.967 1.481-8.279-6.064-5.828 8.332-1.151z" /></svg>
+      </div>
+      <p class="font-semibold">1220kr</p>
     </div>
   </div>
   <div>
-    <img src="https://tailcake.netlify.app/images/commerce/4.jpg" />
-    <div class="mt-4">
-      <a href="#" class="hover:text-blue-500">Shirt</a>
-      <p class="text-sm text-gray-500 dark:text-gray-300">Navy Blue</p>
-      <p class="text-xl">1220kr</p>
+    <a href="#"><img src="https://tailcake.netlify.app/images/commerce/4.jpg" class="hover:shadow-xl" /></a>
+    <div class="mt-4 text-center">
+      <a href="#" class="text-xl text-slate-500 hover:text-blue-500">Shirt</a>
+
+      <div class="flex justify-center my-2">
+        <svg fill="#ffa41d" class="h-4" xmlns="http://www.w3.org/2000/svg" width="16" height="16" viewBox="0 0 24 24"><path d="M12 .587l3.668 7.568 8.332 1.151-6.064 5.828 1.48 8.279-7.416-3.967-7.417 3.967 1.481-8.279-6.064-5.828 8.332-1.151z" /></svg>
+        <svg fill="#ffa41d" class="h-4" xmlns="http://www.w3.org/2000/svg" width="16" height="16" viewBox="0 0 24 24"><path d="M12 .587l3.668 7.568 8.332 1.151-6.064 5.828 1.48 8.279-7.416-3.967-7.417 3.967 1.481-8.279-6.064-5.828 8.332-1.151z" /></svg>
+        <svg fill="#ffa41d" class="h-4" xmlns="http://www.w3.org/2000/svg" width="16" height="16" viewBox="0 0 24 24"><path d="M12 .587l3.668 7.568 8.332 1.151-6.064 5.828 1.48 8.279-7.416-3.967-7.417 3.967 1.481-8.279-6.064-5.828 8.332-1.151z" /></svg>
+        <svg fill="#edeef1" class="h-4" xmlns="http://www.w3.org/2000/svg" width="16" height="16" viewBox="0 0 24 24"><path d="M12 .587l3.668 7.568 8.332 1.151-6.064 5.828 1.48 8.279-7.416-3.967-7.417 3.967 1.481-8.279-6.064-5.828 8.332-1.151z" /></svg>
+        <svg fill="#edeef1" class="h-4" xmlns="http://www.w3.org/2000/svg" width="16" height="16" viewBox="0 0 24 24"><path d="M12 .587l3.668 7.568 8.332 1.151-6.064 5.828 1.48 8.279-7.416-3.967-7.417 3.967 1.481-8.279-6.064-5.828 8.332-1.151z" /></svg>
+      </div>
+      <p class="font-semibold">1220kr</p>
     </div>
   </div>
   <div>
-    <img src="https://tailcake.netlify.app/images/commerce/5.jpg" />
-    <div class="mt-4">
-      <a href="#" class="hover:text-blue-500">Shirt</a>
-      <p class="text-sm text-gray-500 dark:text-gray-300">Navy Blue</p>
-      <p class="text-xl">1220kr</p>
+    <a href="#"><img src="https://tailcake.netlify.app/images/commerce/5.jpg" class="hover:shadow-xl" /></a>
+    <div class="mt-4 text-center">
+      <a href="#" class="text-xl text-slate-500 hover:text-blue-500">Shirt</a>
+
+      <div class="flex justify-center my-2">
+        <svg fill="#ffa41d" class="h-4" xmlns="http://www.w3.org/2000/svg" width="16" height="16" viewBox="0 0 24 24"><path d="M12 .587l3.668 7.568 8.332 1.151-6.064 5.828 1.48 8.279-7.416-3.967-7.417 3.967 1.481-8.279-6.064-5.828 8.332-1.151z" /></svg>
+        <svg fill="#ffa41d" class="h-4" xmlns="http://www.w3.org/2000/svg" width="16" height="16" viewBox="0 0 24 24"><path d="M12 .587l3.668 7.568 8.332 1.151-6.064 5.828 1.48 8.279-7.416-3.967-7.417 3.967 1.481-8.279-6.064-5.828 8.332-1.151z" /></svg>
+        <svg fill="#ffa41d" class="h-4" xmlns="http://www.w3.org/2000/svg" width="16" height="16" viewBox="0 0 24 24"><path d="M12 .587l3.668 7.568 8.332 1.151-6.064 5.828 1.48 8.279-7.416-3.967-7.417 3.967 1.481-8.279-6.064-5.828 8.332-1.151z" /></svg>
+        <svg fill="#ffa41d" class="h-4" xmlns="http://www.w3.org/2000/svg" width="16" height="16" viewBox="0 0 24 24"><path d="M12 .587l3.668 7.568 8.332 1.151-6.064 5.828 1.48 8.279-7.416-3.967-7.417 3.967 1.481-8.279-6.064-5.828 8.332-1.151z" /></svg>
+        <svg fill="#ffa41d" class="h-4" xmlns="http://www.w3.org/2000/svg" width="16" height="16" viewBox="0 0 24 24"><path d="M12 .587l3.668 7.568 8.332 1.151-6.064 5.828 1.48 8.279-7.416-3.967-7.417 3.967 1.481-8.279-6.064-5.828 8.332-1.151z" /></svg>
+      </div>
+      <p class="font-semibold">1220kr</p>
     </div>
   </div>
   <div>
-    <img src="https://tailcake.netlify.app/images/commerce/6.jpg" />
-    <div class="mt-4">
-      <a href="#" class="hover:text-blue-500">Shirt</a>
-      <p class="text-sm text-gray-500 dark:text-gray-300">Navy Blue</p>
-      <p class="text-xl">1220kr</p>
+    <a href="#"><img src="https://tailcake.netlify.app/images/commerce/6.jpg" class="hover:shadow-xl" /></a>
+    <div class="mt-4 text-center">
+      <a href="#" class="text-xl text-slate-500 hover:text-blue-500">Shirt</a>
+
+      <div class="flex justify-center my-2">
+        <svg fill="#ffa41d" class="h-4" xmlns="http://www.w3.org/2000/svg" width="16" height="16" viewBox="0 0 24 24"><path d="M12 .587l3.668 7.568 8.332 1.151-6.064 5.828 1.48 8.279-7.416-3.967-7.417 3.967 1.481-8.279-6.064-5.828 8.332-1.151z" /></svg>
+        <svg fill="#ffa41d" class="h-4" xmlns="http://www.w3.org/2000/svg" width="16" height="16" viewBox="0 0 24 24"><path d="M12 .587l3.668 7.568 8.332 1.151-6.064 5.828 1.48 8.279-7.416-3.967-7.417 3.967 1.481-8.279-6.064-5.828 8.332-1.151z" /></svg>
+        <svg fill="#ffa41d" class="h-4" xmlns="http://www.w3.org/2000/svg" width="16" height="16" viewBox="0 0 24 24"><path d="M12 .587l3.668 7.568 8.332 1.151-6.064 5.828 1.48 8.279-7.416-3.967-7.417 3.967 1.481-8.279-6.064-5.828 8.332-1.151z" /></svg>
+        <svg fill="#ffa41d" class="h-4" xmlns="http://www.w3.org/2000/svg" width="16" height="16" viewBox="0 0 24 24"><path d="M12 .587l3.668 7.568 8.332 1.151-6.064 5.828 1.48 8.279-7.416-3.967-7.417 3.967 1.481-8.279-6.064-5.828 8.332-1.151z" /></svg>
+        <svg fill="#edeef1" class="h-4" xmlns="http://www.w3.org/2000/svg" width="16" height="16" viewBox="0 0 24 24"><path d="M12 .587l3.668 7.568 8.332 1.151-6.064 5.828 1.48 8.279-7.416-3.967-7.417 3.967 1.481-8.279-6.064-5.828 8.332-1.151z" /></svg>
+      </div>
+      <p class="font-semibold">1220kr</p>
     </div>
   </div>
 </div>
