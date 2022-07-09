511acd77d359c84de5b144af6786f5276bb6185f
Matus Stafura <kobe21@gmail.com>
2022-07-09 08:54:25 +0200

    refactor; patch responsive sidebar

--- src/Sidebar.vue
+++ src/Sidebar.vue
@@ -56,6 +56,63 @@
       >Tables</router-link
     >
   </div>
+  <div
+    id="sidebar"
+    :class="[
+      visible
+        ? 'hidden p-4'
+        : 'bg-white w-64 border h-screen shadow-xl lg:hidden max-h-screen px-8 fixed top-16 z-20 p-4',
+    ]"
+  >
+    <router-link to="alerts" class="block mb-4 hover:underline"
+      >Alerts</router-link
+    >
+    <router-link to="blog" class="block mb-4 hover:underline">Blog</router-link>
+    <router-link to="breadcrumbs" class="block mb-4 hover:underline"
+      >Breadcrumbs</router-link
+    >
+    <router-link to="buttons" class="block mb-4 hover:underline"
+      >Buttons</router-link
+    >
+    <router-link to="cards" class="block mb-4 hover:underline"
+      >Cards</router-link
+    >
+    <router-link to="commerce" class="block mb-4 hover:underline"
+      >Commerce</router-link
+    >
+    <router-link to="faq" class="block mb-4 hover:underline">FAQ</router-link>
+    <router-link to="cta" class="block mb-4 hover:underline">CTA</router-link>
+    <router-link to="footer" class="block mb-4 hover:underline"
+      >Footers</router-link
+    >
+    <router-link to="forms" class="block mb-4 hover:underline"
+      >Forms</router-link
+    >
+    <router-link to="navbars" class="block mb-4 hover:underline"
+      >Navbars</router-link
+    >
+    <router-link to="notifications" class="block mb-4 hover:underline"
+      >Notifications</router-link
+    >
+    <router-link to="pagination" class="block mb-4 hover:underline"
+      >Pagination</router-link
+    >
+    <router-link to="pricing" class="block mb-4 hover:underline"
+      >Pricing</router-link
+    >
+    <router-link to="ratings" class="block mb-4 hover:underline"
+      >Ratings</router-link
+    >
+    <router-link to="sidebar" class="block mb-4 hover:underline"
+      >Sidebar</router-link
+    >
+    <router-link to="steps" class="block mb-4 hover:underline"
+      >Steps</router-link
+    >
+    <router-link to="tables" class="block mb-4 hover:underline"
+      >Tables</router-link
+    >
+  </div>
 </template>
 
 <script>
--- src/components/Element.vue
+++ src/components/Element.vue
@@ -10,65 +10,19 @@
       <h2 class="justify-start text-xl font-semibold">{{ title }}</h2>
       <div class="flex mt-0">
         <button @click="showCode" class="ml-4">
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            class="w-6 h-6"
-            fill="none"
-            viewBox="0 0 24 24"
-            stroke="currentColor"
-          >
-            <path
-              stroke-linecap="round"
-              stroke-linejoin="round"
-              stroke-width="2"
-              d="M10 20l4-16m4 4l4 4-4 4M6 16l-4-4 4-4"
-            />
-          </svg>
+          <ShowCode></ShowCode>
         </button>
         <button @click="darkMode" class="ml-4">
-          <svg
-            v-if="!dark"
-            xmlns="http://www.w3.org/2000/svg"
-            class="w-6 h-6"
-            fill="none"
-            viewBox="0 0 24 24"
-            stroke="currentColor"
-          >
-            <path
-              stroke-linecap="round"
-              stroke-linejoin="round"
-              stroke-width="2"
-              d="M20.354 15.354A9 9 0 018.646 3.646 9.003 9.003 0 0012 21a9.003 9.003 0 008.354-5.646z"
-            />
-          </svg>
-          <svg
-            v-else
-            xmlns="http://www.w3.org/2000/svg"
-            class="w-6 h-6"
-            viewBox="0 0 20 20"
-            fill="currentColor"
-          >
-            <path
-              d="M17.293 13.293A8 8 0 016.707 2.707a8.001 8.001 0 1010.586 10.586z"
-            />
-          </svg>
+          <div v-if="!dark">
+            <DarkModeLight></DarkModeLight>
+          </div>
+          <div v-else>
+            <DarkModeDark></DarkModeDark>
+          </div>
         </button>
 
         <button @click="copyCode" class="ml-4">
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            class="w-6 h-6"
-            fill="none"
-            viewBox="0 0 24 24"
-            stroke="currentColor"
-          >
-            <path
-              stroke-linecap="round"
-              stroke-linejoin="round"
-              stroke-width="2"
-              d="M8 5H6a2 2 0 00-2 2v12a2 2 0 002 2h10a2 2 0 002-2v-1M8 5a2 2 0 002 2h2a2 2 0 002-2M8 5a2 2 0 012-2h2a2 2 0 012 2m0 0h2a2 2 0 012 2v3m2 4H10m0 0l3-3m-3 3l3 3"
-            />
-          </svg>
+          <CopyButton></CopyButton>
         </button>
       </div>
     </div>
@@ -87,6 +41,10 @@
 </template>
 
 <script>
+import CopyButton from "@/svgs/CopyButton.vue";
+import ShowCode from "../svgs/ShowCode.vue";
+import DarkModeLight from "../svgs/DarkModeLight.vue";
+import DarkModeDark from "../svgs/DarkModeDark.vue";
 export default {
   props: ["title", "code"],
   data() {
@@ -112,6 +70,7 @@ export default {
       });
     },
   },
+  components: { CopyButton, ShowCode, DarkModeLight, DarkModeDark },
 };
 </script>
 
