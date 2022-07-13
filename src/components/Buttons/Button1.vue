361d143ff5fe946cc373233e6c9caa8b63cc3b30
Matus Stafura <kobe21@gmail.com>
2022-07-13 10:30:06 +0200

    improve buttons

--- src/components/Buttons/Button1.vue
+++ src/components/Buttons/Button1.vue
@@ -10,15 +10,7 @@ export default {
   data() {
     return {
       code: `
-<button class="px-6 py-3 m-2 text-white uppercase bg-indigo-400 rounded shadow-lg hover:bg-indigo-500 dark:bg-gray-500 dark:text-gray-300 dark:hover:bg-gray-400 dark:hover:text-gray-100">Start now</button>
-<button class="px-6 py-3 m-2 text-indigo-500 uppercase border rounded shadow-lg bg-white-400 border-gray-50 hover:text-indigo-400 dark:text-gray-300 dark:hover:bg-gray-500 dark:hover:text-gray-100">Contact us</button>
-<button class="px-4 py-3 m-2 text-white bg-blue-400 rounded shadow-md hover:bg-blue-500 dark:hover:bg-blue-500 dark:hover:text-gray-100">Read more</button>
-
-<button class="px-6 py-2 m-2 text-white bg-indigo-500 rounded-3xl hover:bg-black">
-  <span>Get in touch</span> <svg fill="#ffffff" class="inline w-3 ml-2 -mt-1 text-white" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"><path d="M7.33 24l-2.83-2.829 9.339-9.175-9.339-9.167 2.83-2.829 12.17 11.996z" /></svg>
-</button>
-
-<button class="px-20 py-2 m-2 text-white rounded-md bg-emerald-400 hover:bg-black"><span>Subscribe</span></button>
+<button class="px-6 py-3 bg-blue-600 rounded shadow-md text-blue-50 hover:bg-blue-500 dark:hover:bg-blue-400">Read more</button>
       `,
       title: "Primary Button",
     };
--- src/components/Buttons/Button6.vue
+++ src/components/Buttons/Button6.vue
@@ -10,19 +10,17 @@ export default {
   data() {
     return {
       code: `
-
-<div class="flex items-center">
-  <div class="w-64 group">
+<div class="flex items-center gap-x-20">
+  <div class="group">
     <div class="absolute hidden p-3 text-yellow-800 rounded shadow -mt-14 bg-yellow-50 group-hover:block">Here's more information.</div>
-    <button class="block w-32 px-4 py-3 text-white bg-blue-400 rounded-md">hover me</button>
+    <button class="block w-32 px-4 py-3 text-white bg-blue-600 rounded-md hover:bg-blue-500">hover me</button>
   </div>
 
-  <div class="w-64 group">
+  <div class="group">
     <div class="absolute hidden p-3 text-yellow-800 rounded shadow -mt-14 bg-yellow-50 group-hover:block">Here's more information.</div>
     <a class="text-black underline decoration-dotted dark:text-blue-400">more info</a>
   </div>
-</div>
-      
+</div>    
       `,
       title: "Tooltip",
     };
